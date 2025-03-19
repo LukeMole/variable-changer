1f9a205e735120308db8b2d996d1048ee1697e43
LukeMole <93032440+LukeMole@users.noreply.github.com>
2025-03-19 12:08:09 +0200

    added variable name changer

--- variable_name_changer.py
+++ variable_name_changer.py
@@ -1,4 +1,6 @@
 import os
+import random
+from PyMultiDictionary import MultiDictionary
 
 def fetch_variables(file_path):
     with open(file_path, 'r') as file:
@@ -41,32 +43,54 @@ def split_var_words(variables, case_type):
 
         for var in variables:
             if '_' in var:
-                split_vars.append(var.split('_'))
+                split_vars.append({var:var.split('_')})
             else:
-                split_vars.append([var])
+                split_vars.append({var:[var]})
     
     elif case_type == 'camel':
         for var in variables:
+            unchanged_var = var
             for I in range(len(var)):
                 if var[I].isupper():
                     var = var.replace(var[I], f' {var[I].lower()}')
-            var = var.strip()
-            print(var)    
-
-                
-            split_vars.append(var)
+            var = var.strip()  
+            if ' ' in var:
+                split_vars.append({unchanged_var:var.split(' ')})
+            else:
+                split_vars.append({unchanged_var:[var]})
 
     return split_vars
 
+def rename_variables(var_dicts):
+    dictionary = MultiDictionary()
+    #print(dictionary.synonym('en',"good"))
+    new_vars = {}
+    for var_dict in var_dicts:
+        new_var_words = []
+        for var_name in var_dict:
+            for word in var_dict[var_name]:
+                new_words = dictionary.synonym('en', word)
+                if len(new_words) == 0:
+                    new_word = word
+                else:
+                    new_word = new_words[random.randint(0, len(new_words)-1)]
+                print(new_word)
+                new_var_words.append(new_word)
+            
+            new_vars = new_vars | {var_name : new_var_words}
+    
+    print(new_vars)
+
 if __name__ == "__main__":
-    file_path = "test_camel.js"
+    file_path = "test_snake.py"
     variables = fetch_variables(file_path)
     print(len(variables))
     for variable in variables:
         #print(variable)
         ...
 
-    split_vars = split_var_words(variables, 'camel')
+    split_vars = split_var_words(variables, 'snake')
     print(split_vars)
+    rename_variables(split_vars)
 
     #create_new_names(variables, 'S')
